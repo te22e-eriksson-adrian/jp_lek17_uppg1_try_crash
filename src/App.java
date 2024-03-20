6a2569cd986e8955f65a71d42439bdfe20f7720c
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-03-20 10:23:39 +0100

    lade till koden och länkade till javabibliotek samt testade att krasha programmet

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ange vad 10 ska delas");
+        int var1 = 10;
+        int var2 = scan.nextInt();
+    }
+}

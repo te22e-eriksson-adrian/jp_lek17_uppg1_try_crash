d9ae4f859f621640ea902c538acb4c3aed22e437
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-03-20 10:57:44 +0100

    ändrade i loop-huvudet för att lösa problemet - finish

--- src/App.java
+++ src/App.java
@@ -16,5 +16,23 @@ public class App {
         double svar = ((double)var1) / var2;
         System.out.println("10 delat med "+var2+" är "+ svar);
         scan.close();
+        System.out.println();
+
+        int[] fält = {6,3,5,9};
+        
+        for (int i = 0; i < fält.length; i++) {
+            System.out.println("element nr "+i+" har värdet "+fält[i]);
+        }
+
+        /* //enligt uppgift
+        try{
+            for (int i = 0; i < 10; i++) {
+                System.out.println("element nr "+i+" har värdet "+fält[i]);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Du måste ändra till fält.lenght istället för 10.");
+        }
+         */
     }
 }

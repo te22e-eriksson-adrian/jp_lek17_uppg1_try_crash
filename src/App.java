18788bf6dfa45c263f79a834268d19b312081be8
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-03-20 10:48:22 +0100

    jag hoppar över del2 då det visar sig att den är anpassad till en annan version av java

--- src/App.java
+++ src/App.java
@@ -5,12 +5,16 @@ public class App {
         Scanner scan = new Scanner(System.in);
         System.out.println("Ange vad 10 ska delas");
         int var1 = 10;
+        int var2 = 0;
         try{
-            int var2 = scan.nextInt();
+            var2 = scan.nextInt();
         }
         catch(Exception e){
             System.out.println("Felaktig inmatning, använd siffror");
         }
+
+        double svar = ((double)var1) / var2;
+        System.out.println("10 delat med "+var2+" är "+ svar);
         scan.close();
     }
 }

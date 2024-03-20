775ce5210a0d66adeef22d2ca413116703e929e0
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-03-20 10:29:22 +0100

    fixade en try-catch funktion till programmet så att det inte kraschar

--- src/App.java
+++ src/App.java
@@ -5,6 +5,12 @@ public class App {
         Scanner scan = new Scanner(System.in);
         System.out.println("Ange vad 10 ska delas");
         int var1 = 10;
-        int var2 = scan.nextInt();
+        try{
+            int var2 = scan.nextInt();
+        }
+        catch(Exception e){
+            System.out.println("Felaktig inmatning, använd siffror");
+        }
+        scan.close();
     }
 }
